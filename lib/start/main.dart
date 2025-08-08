6f4eb5f6f9dca20fc8a0fc05601f51e854e4b470
Levent <leventergoren2005@hotmail.com>
2025-08-08 16:07:21 +0300

    Crashlytics eklendi

--- lib/start/main.dart
+++ lib/start/main.dart
@@ -1,11 +1,25 @@
 import 'package:ders_app/core/app_bindings.dart';
+import 'package:ders_app/firebase_options.dart';
 import 'package:ders_app/modules/routes/app_pages.dart';
 import 'package:ders_app/themes/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   runApp(const MyApp());
 }
 
